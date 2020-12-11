9e801714a8165838f96fbb38549a1fc82668ff5a
Markus Lorensen <293106@VIAUC.dk>
2020-12-11 10:30:29 +0100

    Jsonformat fix

--- src/Tier3/DataServer/Models/PurchaseRequest.java
+++ src/Tier3/DataServer/Models/PurchaseRequest.java
@@ -49,7 +49,7 @@ public class PurchaseRequest {
     return "{"
         + "\"bookSale\":" + "\"" + getBookSale() + "\","
         + "\"buyer\":" + "\"" + getBuyer() + "\","
-        + "\"seller\":" + "\"" + getSeller()
+        + "\"seller\":" + "\"" + getSeller() + "\""
         + "}";
   }
 }
